09ba8f31ccd5372c32f291846db768eb918c26cf
harshithk1218 <harshithkbca24@rvu.edu.in>
2025-02-05 15:44:55 +0530

    Update and rename prg-8 to prg-8.py

--- lab3/prg-8.py
+++ lab3/prg-8.py
@@ -3,8 +3,7 @@ start = time.time()
 squares = [x ** 2 for x in range(1,100000001)]
 end = time.time()
 print("List comprehension time:",end - start)
-print(sys.getsizeof(squares)) #
-# Using traditional for loop
+print(sys.getsizeof(squares)) 
 start = time.time()
 squares = []
 for x in range(1, 1000001):
