70c891de9b7e7928d7568d1da40cdce47b42030b
harshith k <harshithkbca24@rvu.edu.in>
2025-04-09 10:32:23 +0530

    Add files via upload

---
+++ Lab3/Creating_Strings.py
@@ -0,0 +1,4 @@
+str = 'Hello' # single quotes
+print(str)
+str_2 = "Hello" # double quotes
+print(str_2)
---
+++ Lab3/List_Id_Sort.py
@@ -0,0 +1,5 @@
+lst = [10, 2, 4, 1, 6]
+print(lst)
+lst.sort()
+print(lst)
+print(id(lst),id(lst.sort()))
